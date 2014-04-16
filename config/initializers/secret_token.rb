5b6d4a263c554dfff305ef5205ab6aa8fffe3b1a
Jan Ahrens <jan.ahrens+git@googlemail.com>
2014-04-16 22:04:37 +0200

    load secret token from environment in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Canvas::Application.config.secret_token = '67928a24146331ff48a7401b90ba8b4baa10eb6e475020320243b1098836489a6c4b1614a3bffc9da192c7e2263067955ecd12274c82dd6d89d6a0fbdbfa73f8'
+
+# source: http://daniel.fone.net.nz/blog/2013/05/20/a-better-way-to-manage-the-rails-secret-token/
+Canvas::Application.config.secret_token = if Rails.env.development? || Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
